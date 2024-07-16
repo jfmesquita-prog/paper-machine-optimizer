e5f36a82c908077a7a85cdfd63100e79096dd612
jfmesquita-prog <mesquitinha7@gmail.com>
2024-07-16 12:18:55 +0100

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,6 +1,3 @@
-const axios = require('axios');
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxsyy8zD8hd3ZoycKgAOrJ9M02O5zmJcXisuaTiy-NQ2c1IrlF9kAE8UaRzdCPaka--/exec';
-
 exports.handler = async function(event, context) {
   console.log('Função proxy iniciada');
   console.log('Método HTTP:', event.httpMethod);
@@ -14,16 +11,13 @@ exports.handler = async function(event, context) {
     const body = JSON.parse(event.body);
     console.log('Dados enviados para o Google Apps Script:', body);
     
-    const response = await axios.post(SCRIPT_URL, body, {
-      headers: { 'Content-Type': 'application/json' }
-    });
+    const response = await axios.post(SCRIPT_URL, body);
     
     console.log('Resposta bruta do Google Apps Script:', response.data);
 
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(response.data)
@@ -33,7 +27,6 @@ exports.handler = async function(event, context) {
     return {
       statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ 
